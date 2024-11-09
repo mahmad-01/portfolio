11866afc8aa3a51be66ec9c886f1116fb3c26699
mahmad-01 <mahmadbcs@gmail.com>
2024-11-09 14:19:32 -0400

    Stage 1 almost completeeeee

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { KeyboardEvent, useRef, useState } from "react";
 
 export default function Home() {
   const inputRef = useRef<HTMLInputElement>(null);
-  const [history, setHistory] = useState<string[]>([]);
+  const [history, setHistory] = useState<{text: string, color: string, isCommand?: boolean}[]>([]);
   const [input, setInput] = useState<string>("");
   const commands = ["whoami", "help", "neofetch", "clear", ""];
 
@@ -16,7 +16,7 @@ export default function Home() {
       const output = handleCommand(input);
       if (input !== "clear"){
         setHistory((prevHistory)=> {
-          return [...prevHistory, `$ ${input}`, output!];
+          return [...prevHistory, {text: `${input}`, color: "#C691A3", isCommand: true}, {text: output!, color:"white"}];
         });
       }
         setInput("");
@@ -25,12 +25,19 @@ export default function Home() {
   function handleCommand(command: string){
     if (commands.includes(command)){
       switch (command){
+        
         case "whoami":
           return "root";
+        
         case "help":
             return "commands: help whoami neofetch clear (hint: use neofetch)"; 
+        
         case "clear":
           setHistory([]);
+        
+         case "neofetch":
+          return ""
+
         }
         
       } else {
@@ -39,7 +46,7 @@ export default function Home() {
   }
 
   return (
-    <div className=" h-screen w-full justify-center align-center p-6">
+    <div className="h-screen w-full justify-center align-center p-6">
       <div className="flex flex-col gap-2 bg-[#1E1E2E] h-4/6 opacity-70 py-6 px-4 rounded-lg border-[#6B85BC] border w-full" onClick={()=>inputRef.current?.focus()}>
       <div className="flex flex-row gap-2" >
       <p>
@@ -50,14 +57,18 @@ export default function Home() {
       </p>
       </div>
       {history.map((line, index)=>(
-        <div key={index}>
-          {line}
+        <div key={index} className={` flex flex-row gap-2 text-[${line.color}]`} >
+          {line.isCommand && (
+            <p className="text-white"> $ </p>
+          )}
+          {line.text}
           </div>
       ))}
       <div className="flex flex-row">
        <p className="weightSmall pr-2"> {"$"} </p> 
-       <input ref={inputRef} value={input} onChange={handleInputChange} onKeyDown={handleEnter} autoFocus maxLength={30} type="text" className="bg-[#1E1E2E] text-[#6B85BC] outline-none weightLarger"/>
+       <input ref={inputRef} value={input} onChange={handleInputChange} onKeyDown={handleEnter} autoFocus maxLength={30} type="text" className="bg-[#1E1E2E] text-[#C691A3] outline-none weightLarger"/>
       </div>
+      
       </div>
     </div>
   );
